a1624cf4c306c40167b923b1926c33319489a158
chompaa <antony.m.3012@gmail.com>
2020-01-02 15:22:26 +0000

    a_star func -> grid struct

--- src/main.rs
+++ src/main.rs
@@ -124,76 +124,76 @@ impl Grid {
 
     neighbours
   }
-}
 
-fn a_star(grid: Grid, mut start: Node, goal: Node) -> Vec<Node> {
-  let mut open_nodes: Vec<Node> = vec![start];
-  let mut closed_nodes: Vec<Node> = Vec::new();
-  let mut current_node: Node;
+  fn a_star(&self, mut start: Node, goal: Node) -> Vec<Node> {
+    let mut open_nodes: Vec<Node> = vec![start];
+    let mut closed_nodes: Vec<Node> = Vec::new();
+    let mut current_node: Node;
 
-  start.set_h(goal);
-  start.f = start.g + start.h;
+    start.set_h(goal);
+    start.f = start.g + start.h;
 
-  while open_nodes.len() > 0 {
-    open_nodes.sort_by(|x, y| x.f.cmp(&y.f));
+    while open_nodes.len() > 0 {
+      open_nodes.sort_by(|x, y| x.f.cmp(&y.f));
 
-    current_node = open_nodes.swap_remove(0);
-    closed_nodes.push(current_node);
+      current_node = open_nodes.swap_remove(0);
+      closed_nodes.push(current_node);
 
-    if current_node.index == goal.index {
-      return get_path(closed_nodes, current_node);
-    }
+      if current_node.index == goal.index {
+        return self.get_path(closed_nodes, current_node);
+      }
 
-    for neighbour in grid.get_neighbours(current_node).iter_mut() {
-      neighbour.parent_index = current_node.index;
+      for neighbour in self.get_neighbours(current_node).iter_mut() {
+        neighbour.parent_index = current_node.index;
 
-      if !closed_nodes.contains(neighbour) {
-        neighbour.set_g_f(current_node, neighbour.parent_diagonal);
+        if !closed_nodes.contains(neighbour) {
+          neighbour.set_g_f(current_node, neighbour.parent_diagonal);
 
-        if open_nodes.contains(neighbour) {
-          let mut open_neighbour: Node = Node::new(neighbour.index, neighbour.x, neighbour.y);
-          for node in open_nodes.iter() {
-            if node == neighbour {
-              open_neighbour = *node;
+          if open_nodes.contains(neighbour) {
+            let mut open_neighbour: Node = Node::new(neighbour.index, neighbour.x, neighbour.y);
+            for node in open_nodes.iter() {
+              if node == neighbour {
+                open_neighbour = *node;
+              }
             }
-          }
 
-          if neighbour.g < open_neighbour.g {
-            open_neighbour.g = neighbour.g;
-            open_neighbour.parent_index = neighbour.parent_index;
+            if neighbour.g < open_neighbour.g {
+              open_neighbour.g = neighbour.g;
+              open_neighbour.parent_index = neighbour.parent_index;
+            }
+          } else {
+            open_nodes.push(*neighbour);
           }
-        } else {
-          open_nodes.push(*neighbour);
         }
       }
     }
+
+    Vec::new()
   }
 
-  Vec::new()
-}
+  fn get_path(&self, closed_nodes: Vec<Node>, mut node: Node) -> Vec<Node> {
+    let mut path: Vec<Node> = vec![node];
 
-fn get_path(closed_nodes: Vec<Node>, mut node: Node) -> Vec<Node> {
-  let mut path: Vec<Node> = vec![node];
-
-  while node.parent_index != -1 {
-    for closed_node in closed_nodes.iter() {
-      if closed_node.index == node.parent_index {
-        node = *closed_node;
+    while node.parent_index != -1 {
+      for closed_node in closed_nodes.iter() {
+        if closed_node.index == node.parent_index {
+          node = *closed_node;
+        }
       }
+
+      path.push(node);
     }
 
-    path.push(node);
+    path.reverse();
+    path
   }
-
-  path.reverse();
-  path
 }
 
 fn main() {
   let grid: Grid = Grid::new(7, 7, vec![5, 10, 12, 17, 19, 26, 31, 38, 40, 45, 47]);
   let nodes = grid.get_nodes();
   let width = grid.get_width();
-  let path: Vec<Node> = a_star(grid, nodes[42], nodes[6]);
+  let path: Vec<Node> = grid.a_star(nodes[42], nodes[6]);
 
   print!("Path: ");
   for (index, node) in path.iter().enumerate() {
